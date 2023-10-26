bbabd50470714f30668c51e3034da0a57bf2cc6f
Aubrey042003 <113245153+Aubrey042003@users.noreply.github.com>
2023-10-26 08:47:54 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSongLyrics.java
@@ -0,0 +1,14 @@
+public class FavoriteSongLyrics {
+    public static void main(String[] args) {
+    
+    FavoriteSongLyrics lyrics = new FavoriteSongLyrics();
+    lyrics.displaySongLyrics();
+    }
+
+        public void displaySongLyrics() {
+        System.out.println("I'm on the highway to hell");
+        System.out.println("On the highway to hell");
+        System.out.println("Highway to hell");
+        System.out.println("I'm on the highway to hell");
+    }
+}
